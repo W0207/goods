22edac5e860313f52b3f71332f4f88dde58c84f8
Tristanxxy <1481039399@qq.com>
2020-11-30 21:56:05 +0800

    1130shop

--- goodsservice/src/main/java/cn/edu/xmu/goods/model/bo/Shop.java
+++ goodsservice/src/main/java/cn/edu/xmu/goods/model/bo/Shop.java
@@ -1,17 +1,110 @@
 package cn.edu.xmu.goods.model.bo;
 
-import cn.edu.xmu.ooad.model.VoObject;
+
+import cn.edu.xmu.goods.model.po.ShopPo;
+import cn.edu.xmu.goods.model.vo.newShopRetVo;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
-public class Shop implements VoObject {
-    @Override
-    public Object createVo() {
-        return null;
+public class Shop {
+
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime gmtCreate;
+
+    private LocalDateTime gmtModified;
+
+    private State state = State.NEW;
+
+    public void setId(Long id) {
+        this.id=id;
     }
 
-    @Override
-    public Object createSimpleVo() {
-        return null;
+    /**
+     * 店铺状态
+     */
+    public enum State {
+        NEW(0, "未审核"),
+        UP(1, "上线"),
+        DOWN(2, "未上线"),
+        CLOSE(3, "关闭");
+
+        private static final Map<Integer, Shop.State> stateMap;
+
+        static { //由类加载机制，静态块初始加载对应的枚举属性到map中，而不用每次取属性时，遍历一次所有枚举值
+            stateMap = new HashMap();
+            for (Shop.State enum1 : values()) {
+                stateMap.put(enum1.code, enum1);
+            }
+        }
+
+        private int code;
+        private String description;
+
+        State(int code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public static Shop.State getTypeByCode(Integer code) {
+            return stateMap.get(code);
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
+
+    /**
+     * 构造函数
+     * 用po构造
+     */
+    public Shop(ShopPo po)
+    {
+        this.id=po.getId();
+        this.name=po.getName();
+        this.gmtCreate=po.getGmtCreated();
+        this.gmtModified=po.getGmtModified();
+    }
+
+    public Shop(String name)
+    {
+        this.name=name;
+    }
+    public Shop()
+    {
+
+    }
+
+    public ShopPo getShopPo(){
+        ShopPo shopPo= new ShopPo();
+        shopPo.setId(this.id);
+        shopPo.setName(this.name);
+        shopPo.setGmtCreated(this.gmtCreate);
+        shopPo.setGmtModified(this.gmtModified);
+        Byte state = (byte) this.state.code;
+        shopPo.setState(state);
+        return  shopPo;
+    }
+
+    /*public newShopRetVo getShopRetPo(){
+        newShopRetVo vo= new newShopRetVo();
+        vo.setId(this.id);
+        vo.setName(this.name);
+        vo.setGmtCreated(this.gmtCreate);
+        vo.setGmtModified(this.gmtModified);
+        return  vo;
+    }*/
+
+
 }
---
+++ goodsservice/src/test/java/cn/edu/xmu/goods/Controller/ShopTest.java
@@ -0,0 +1,77 @@
+package cn.edu.xmu.goods.Controller;
+
+import cn.edu.xmu.goods.GoodsServiceApplication;
+import cn.edu.xmu.goods.model.vo.ShopVo;
+import cn.edu.xmu.ooad.util.JacksonUtil;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = GoodsServiceApplication.class)   //标识本类是一个SpringBootTest
+@AutoConfigureMockMvc    //配置模拟的MVC，这样可以不启动服务器测试
+@Transactional
+public class ShopTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void inserShopTest()
+    {
+        ShopVo vo =new ShopVo();
+        vo.setName("test");
+        String shopJson = JacksonUtil.toJson(vo);
+        String expectedResponse = "";
+        String responseString = null;
+        try {
+            responseString = this.mvc.perform(post("/shop/shops").contentType("application/json;charset=UTF-8").content(shopJson))
+                    .andExpect(status().isCreated())
+                    .andExpect(content().contentType("application/json;charset=UTF-8"))
+                    .andReturn().getResponse().getContentAsString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        expectedResponse = "{\"errno\":0,\"data\":{\"id\":1,\"name\":\"test\",\"gmtModified\":null},\"errmsg\":\"成功\"}";
+        System.out.println(responseString);
+        try {
+            JSONAssert.assertEquals(expectedResponse, responseString, false);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println(responseString);
+
+    }
+
+    @Test
+    public void getAllShopState() throws Exception {
+        String responseString = this.mvc.perform(get("/shop/states"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andReturn().getResponse().getContentAsString();
+        String expectedResponse = "{ \"errno\": 0, \"data\": [ { \"code\": 0,\"name\": \"未审核\" }, { \"name\": \"上线\", \"code\": 1 }, { \"name\": \"未上线\", \"code\": 2 }, { \"name\": \"关闭\", \"code\": 3 }], \"errmsg\": \"成功\" }";
+        JSONAssert.assertEquals(expectedResponse, responseString, false);
+        System.out.println(responseString);
+    }
+
+    @Test
+    public void updateShop() throws Exception {
+        ShopVo vo =new ShopVo();
+        vo.setName("test");
+        String shopJson = JacksonUtil.toJson(vo);
+
+        String responseString = this.mvc.perform(
+                put("/shop/shops/1")
+                        .contentType("application/json;charset=UTF-8").content(shopJson)
+        ).andExpect(status().isOk()).andExpect(content().contentType("application/json;charset=UTF-8")).andReturn().getResponse().getContentAsString();
+        System.out.println(responseString);
+    }
+}
