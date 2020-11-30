bc28dcd096e0dba270cd14bfb5a44972d517dbe6
BiuBiuBia <2549639382@qq.com>
2020-11-30 21:05:13 +0800

    update-查找所有品牌

---
+++ goodsservice/src/main/java/cn/edu/xmu/goods/service/BrandService.java
@@ -0,0 +1,25 @@
+package cn.edu.xmu.goods.service;
+
+import cn.edu.xmu.goods.dao.BrandDao;
+import cn.edu.xmu.ooad.model.VoObject;
+import cn.edu.xmu.ooad.util.ReturnObject;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BrandService {
+
+    @Autowired
+    private BrandDao brandDao;
+
+    private static final Logger logger = LoggerFactory.getLogger(GoodsService.class);
+
+    public ReturnObject<PageInfo<VoObject>> findAllBrand(Integer page, Integer pageSize){
+        return brandDao.findAllBrand(page, pageSize);
+    }
+
+
+}
