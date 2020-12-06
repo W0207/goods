9a1978f1fff5df5381addf64becd22859ffd54d8
W0207 <710495718@qq.com>
2020-12-06 16:43:24 +0800

    update

--- core/src/main/java/cn/edu/xmu/ooad/util/ImgHelper.java
+++ core/src/main/java/cn/edu/xmu/ooad/util/ImgHelper.java
@@ -20,28 +20,28 @@ public class ImgHelper {
      * 保存单个图片并限制大小在远程服务器，直接以multipartFile形式
      *
      * @param multipartFile
-     * @param size          文件最大大小
-     * @param username      dav服务器用户名
-     * @param password      dav服务器密码
-     * @param baseUrl       dav地址及其目录
+     * @param size         文件最大大小
+     * @param username     dav服务器用户名
+     * @param password     dav服务器密码
+     * @param baseUrl      dav地址及其目录
      * @return
      */
     public static ReturnObject remoteSaveImg(MultipartFile multipartFile,
-                                             int size, String username, String password, String baseUrl) throws IOException {
+            int size, String username,String password,String baseUrl) throws IOException {
 
         //判断是否是图片
-        if (!isImg(multipartFile))
+        if(!isImg(multipartFile))
             return new ReturnObject(ResponseCode.IMG_FORMAT_ERROR);
 
         //判断文件大小是否符合要求
-        if (multipartFile.getSize() > size * 1024 * 1024) {
+        if(multipartFile.getSize()>size*1024*1024){
             return new ReturnObject(ResponseCode.IMG_SIZE_EXCEED);
         }
 
-        Sardine sardine = SardineFactory.begin(username, password);
+        Sardine sardine = SardineFactory.begin(username,password);
 
         //没有权限创建则抛出IOException
-        if (!sardine.exists(baseUrl)) {
+        if(!sardine.exists(baseUrl)){
             sardine.createDirectory(baseUrl);
         }
 
@@ -50,7 +50,7 @@ public class ImgHelper {
         String suffix = multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf("."));
         String fileName = UUID.randomUUID() + suffix;
 
-        sardine.put(baseUrl + fileName, fileInputStream);
+        sardine.put(baseUrl+fileName,fileInputStream);
 
         sardine.shutdown();
         return new ReturnObject(fileName);
@@ -61,18 +61,19 @@ public class ImgHelper {
      * 删除远程服务器文件
      *
      * @param filename 文件名
-     * @param username dav服务器用户名
-     * @param password dav服务器密码
-     * @param baseUrl  dav地址及其目录
+     * @param username     dav服务器用户名
+     * @param password     dav服务器密码
+     * @param baseUrl      dav地址及其目录
      */
     public static void deleteRemoteImg(String filename, String username,
-                                       String password, String baseUrl) {
-        Sardine sardine = SardineFactory.begin(username, password);
-        try {
-            sardine.delete(baseUrl + filename);
+                                       String password,String baseUrl){
+        Sardine sardine = SardineFactory.begin(username,password);
+        try{
+            sardine.delete(baseUrl+filename);
             sardine.shutdown();
-        } catch (IOException e) {
-            logger.error("delete fail: " + filename);
+        }
+        catch(IOException e){
+            logger.error("delete fail: "+ filename);
         }
         return;
     }
@@ -81,10 +82,11 @@ public class ImgHelper {
      * 判断文件是否是图片
      *
      * @param multipartFile 文件
+     *
      */
-    public static boolean isImg(MultipartFile multipartFile) throws IOException {
+    public static boolean isImg(MultipartFile multipartFile) throws IOException{
         BufferedImage bi = ImageIO.read(multipartFile.getInputStream());
-        if (bi == null) {
+        if(bi == null){
             return false;
         }
         return true;
