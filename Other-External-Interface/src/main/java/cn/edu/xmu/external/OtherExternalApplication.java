ac86cd5f2927dac1c55d323f2c529165dc43f9ac
W0207 <710495718@qq.com>
2020-12-16 09:18:26 +0800

    update

--- Other-External-Interface/src/main/java/cn/edu/xmu/external/OtherExternalApplication.java
+++ Other-External-Interface/src/main/java/cn/edu/xmu/external/OtherExternalApplication.java
@@ -2,9 +2,10 @@ package cn.edu.xmu.external;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class OtherExternalApplication {
-        public static void main(String[] args) {
-            SpringApplication.run(OtherExternalApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(OtherExternalApplication.class, args);
+    }
 }
